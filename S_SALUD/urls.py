43faa1cd7eb649c23668195f1db716d002de6886
ciosmo <ig.navarroa@duocuc.cl>
2022-06-20 04:37:42 -0400

    viewset agregado, optimizado el codigo

--- S_SALUD/urls.py
+++ S_SALUD/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuario/', include('core.api.urls')),
-    path('products/', include('products.api.urls')),
+    path('products/', include('products.api.routers')),
 ]
---
+++ products/api/routers.py
@@ -0,0 +1,7 @@
+from email.mime import base
+from rest_framework.routers import DefaultRouter
+from products.api.views.product_viewsets import ProductViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='products')
+urlpatterns = router.urls
